df53038ceaa06cae2b71346789c3e0f793a69257
JamesC-Downer <James.Carroll@downer.co.nz>
2026-03-13 12:04:20 +1300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXNjYWRvd25lciIsImEiOiJjbW1uejZzbjMwOG1iMnNva3A1dnVsZ3B1In0.PXVr6LEZejBta20KC40iyw';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jamescadowner/cmmnz0qle00g701speyn3avrl',
+    center: [175.25, -37.78],
+    zoom: 12
+});
+
+// Add nav controls
+map.addControl(new mapboxgl.NavigationControl());
+
+// When user clicks a renewal feature
+map.on('click', (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+        layers: ['bridges-ts']
+    });
+
+    if (!features.length) return;
+
+    const f = features[0];
+    const renewalID = f.properties.system_id;
+
+    // Build the Microsoft Form URL with pre-filled values
+    const formUrl = `https://forms.office.com/Pages/ResponsePage.aspx?ID=4sZL-u3A7EOdbRRefjahpbdh-kCimPNDuvfbFYBkNKlUNVU3MFRSTlZWUzBXRjIyTUxEQkVVRUs5QS4u` +
+        `&rd3642a3a26d141c6a3bb316c9dfe62ce=${encodeURIComponent(renewalID)}` +
+        `&rbf50a96cf4884b49857a7ba85328cbd6=${e.lngLat.lat}&rd4fae6caa7f0416896a745e1042e1b55=${e.lngLat.lng}`;
+
+    // Show a popup
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`
+            <strong>Renewal ID:</strong> ${renewalID}<br>
+            <button onclick="window.open('${formUrl}','_blank')">
+                Add Comment
+            </button>
+        `)
+        .addTo(map);
+});
