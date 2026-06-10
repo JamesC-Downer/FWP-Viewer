6b95dd7137122572a5efc9bf246f34e60b821692
JamesC-Downer <James.Carroll@downer.co.nz>
2026-06-10 13:02:04 +1200

    Update app.js

--- app.js
+++ app.js
@@ -86,6 +86,8 @@ map.on('click', (e) => {
         .setLngLat(e.lngLat)
         .setHTML(`
             <strong>Renewal ID:</strong> ${renewalID}<br>
+            <strong>Planned Year:</strong> ${programme_year}<br>
+            <strong>Treatment:</strong> ${treatment}<br>
             <button onclick="window.open('${formUrl}','_blank')">
                 Add Comment
             </button>
